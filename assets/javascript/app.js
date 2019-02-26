a6ce8c183393cb8f8571a4c7becc4e9ad21d0ca5
Warnerhannah <warnerhannah1@yahoo.com>
2019-02-26 13:11:34 -0800

    basic html and css and array

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+// You'll create a trivia form with multiple choice or true/false options (your choice).
+var myQuestions = [
+    {
+        question: "How long is New Zealand's Ninety Mile Beach?",
+        answers: {
+            a: "55 miles",
+            b: "90 miles",
+            c: "85 miles",
+            d: "92.5 miles",    
+        },
+        correctAnswer: "55 miles",
+    },
+    {
+        question: "What was Walt Disney afraid of?",
+        answers: {
+            a: "Driving",
+            b: "Clowns",
+            c: "Mice",
+            d: "Death",    
+        },
+        correctAnswer: "Mice",
+    },
+    {
+        question: "Who averaged one patent every 3 weeks of their life?",
+        answers: {
+            a: "Thomas Edison",
+            b: "Benjamin Franklin",
+            c: "Sir Isaac Newton",
+            d: "Nikola Tesla",    
+        },
+        correctAnswer: "Thomas Edison",
+    },
+    {
+        question: "How many states border the Gulf of Mexico?",
+        answers: {
+            a: "2",
+            b: "6",
+            c: "3",
+            d: "5",    
+        },
+        correctAnswer: "5",
+    },
+    {
+        question: "What is the most common atmospheric gas?",
+        answers: {
+            a: "Carbon",
+            b: "Nitrogen",
+            c: "Oxygen",
+            d: "Hyrdrogen",    
+        },
+        correctAnswer: "Nitrogen",
+    },
+
+]
+
+
+// The player will have a limited amount of time to finish the quiz. 
+
+
+// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// Don't let the player pick more than one answer per question.
+
+// Don't forget to include a countdown timer. 
+

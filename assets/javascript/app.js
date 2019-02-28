857385bec67e89a6357296b9324642f521df79d1
Warnerhannah <warnerhannah1@yahoo.com>
2019-02-28 15:35:24 -0800

    wed changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var myQuestions = [
             a: "55 miles",
             b: "90 miles",
             c: "85 miles",
-            d: "92.5 miles",    
+            d: "92 miles",    
         },
         correctAnswer: "55 miles",
     },
@@ -53,13 +53,51 @@ var myQuestions = [
 
 ]
 
+//global variables
+var countdown = 30;
+var intervalId;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 
 // The player will have a limited amount of time to finish the quiz. 
+// display time remaining 
+function countDown() {
+    countdown--;
+    $("#countdown").html(countdown);
+    console.log(countdown);
+    if (countdown === 0) {
+        alert("Time is up!");
+        clearInterval(intervalId);
+        scoreGame();
+    }
+}
+intervalId = setInterval(countDown, 1000);
 
 
-// The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+//if they press submit
+$("#submitButton").on("click", function() {
+    scoreGame()
+});
 
-// Don't let the player pick more than one answer per question.
+//The page will reveal the number of questions that players answer correctly and incorrectly.
+//calculate score based on what they clicked
+function scoreGame() {
+    clearInterval(intervalId);
 
-// Don't forget to include a countdown timer. 
+    for (i=0;i<5;i++) {
+        var userChoice = $('input[name=answer]:checked').val()
 
+        if (userChoice === "correct") {
+            correctAnswers++;
+            console.log("right");
+        }
+
+        else {
+            incorrectAnswers++;
+            console.log("wrong")
+        }
+    }
+    alert("Thanks for playing!")
+    alert("You got " + correctAnswers + " answers right, and " + incorrectAnswers + " answers wrong!")
+    $("#correctanswers").html("You got " + correctAnswers + " answers right.")
+};
